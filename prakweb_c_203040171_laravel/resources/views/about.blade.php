ec6ded68fc452e71d5ab951503ab96f36296bfc9
sacred142 <agung.alfatah43@gmail.com>
2022-10-17 18:32:52 +0700

    menyelesaikan video 1 dan 2 laravel 8

---
+++ prakweb_c_203040171_laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WPU BLOG | ABOUT</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>halaman about</h1>
+    <h3><?= $nama ?></h3>
+    <p><?= $email ?></p>
+    <img src="img/<?= $image?>" alt="<?= $nama ?>" width="200">
+</body>
+</html>
