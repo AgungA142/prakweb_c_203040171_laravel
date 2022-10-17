d6062fe693912324a0d56541859bca1f8e1625ff
sacred142 <agung.alfatah43@gmail.com>
2022-10-17 21:09:01 +0700

    menyelesaikan video empat laravel

--- prakweb_c_203040171_laravel/resources/views/about.blade.php
+++ prakweb_c_203040171_laravel/resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU BLOG | ABOUT</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+@extends('layout.main')
+
+@section('container')
     <h1>halaman about</h1>
-    <h3><?= $nama ?></h3>
-    <p><?= $email ?></p>
-    <img src="img/<?= $image?>" alt="<?= $nama ?>" width="200">
-</body>
-</html>
+    <h3>{{ $nama }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $nama }}" width="200">
+@endsection
--- prakweb_c_203040171_laravel/routes/web.php
+++ prakweb_c_203040171_laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Agung Alfatah",
         "email" => "Agung.alfatah43@gmail.com",
         "image" => "1.jpg"
@@ -28,5 +31,55 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+         "title" => "Postingan Pertama",
+         "slug" => "postingan-pertama",
+         "author" => "Agung Alfatah",
+         "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et excepturi illum, explicabo ducimus rem perferendis, incidunt vel sapiente esse consequuntur rerum nulla fuga ratione, magnam totam officia. Fugit eius corporis voluptatem reprehenderit id recusandae nostrum officia voluptates voluptas ab, assumenda ipsum sed provident odit sunt earum nobis ut eum tempore aperiam ratione ea quae mollitia? Voluptate atque ipsam cupiditate ipsa. Dolores magnam ad corrupti magni iste atque quod excepturi velit obcaecati recusandae! Magnam, quibusdam. Cumque minus laudantium modi reprehenderit quo!"
+        ],
+        [
+         "title" => "Postingan Kedua",
+         "slug" => "postingan-kedua",
+         "author" => "Ryan Aja",
+         "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et excepturi illum, explicabo ducimus rem perferendis, incidunt vel sapiente esse consequuntur rerum nulla fuga ratione, magnam totam officia. Fugit eius corporis voluptatem reprehenderit id recusandae nostrum officia voluptates voluptas ab, assumenda ipsum sed provident odit sunt earum nobis ut eum tempore aperiam ratione ea quae mollitia? Voluptate atque ipsam cupiditate ipsa. Dolores magnam ad corrupti magni iste atque quod excepturi velit obcaecati recusandae! Magnam, quibusdam. Cumque minus laudantium modi reprehenderit quo!"
+        ]
+     ];
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => $blog_posts
+    ]);
 });
+
+
+//halaman post single
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+         "title" => "Postingan Pertama",
+         "slug" => "postingan-pertama",
+         "author" => "Agung Alfatah",
+         "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et excepturi illum, explicabo ducimus rem perferendis, incidunt vel sapiente esse consequuntur rerum nulla fuga ratione, magnam totam officia. Fugit eius corporis voluptatem reprehenderit id recusandae nostrum officia voluptates voluptas ab, assumenda ipsum sed provident odit sunt earum nobis ut eum tempore aperiam ratione ea quae mollitia? Voluptate atque ipsam cupiditate ipsa. Dolores magnam ad corrupti magni iste atque quod excepturi velit obcaecati recusandae! Magnam, quibusdam. Cumque minus laudantium modi reprehenderit quo!"
+        ],
+        [
+         "title" => "Postingan Kedua",
+         "slug" => "postingan-kedua",
+         "author" => "Ryan Aja",
+         "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et excepturi illum, explicabo ducimus rem perferendis, incidunt vel sapiente esse consequuntur rerum nulla fuga ratione, magnam totam officia. Fugit eius corporis voluptatem reprehenderit id recusandae nostrum officia voluptates voluptas ab, assumenda ipsum sed provident odit sunt earum nobis ut eum tempore aperiam ratione ea quae mollitia? Voluptate atque ipsam cupiditate ipsa. Dolores magnam ad corrupti magni iste atque quod excepturi velit obcaecati recusandae! Magnam, quibusdam. Cumque minus laudantium modi reprehenderit quo!"
+        ]
+     ];
+     $new_post =[];
+     foreach($blog_posts as $post){
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+     }
+
+
+    return view('post', [
+        "title" => "Blog",
+        "post" => $new_post
+    ]);
+});
+
+
