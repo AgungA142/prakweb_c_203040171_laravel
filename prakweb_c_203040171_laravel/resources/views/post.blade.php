797a9a5e84132c8fbc52521755dd3b712bb884e1
sacred142 <agung.alfatah43@gmail.com>
2022-10-24 22:23:22 +0700

    menyelesaikan video ke 10 laravel

--- prakweb_c_203040171_laravel/resources/views/post.blade.php
+++ prakweb_c_203040171_laravel/resources/views/post.blade.php
@@ -4,7 +4,7 @@
 @section('container')
 <article class="mb-5">
     <h2>{{ $post->title }}</h2>
-    <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a class="text-decoration-none" href="/authors/{{ $post->user->username }}">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
 <a class="d-block mt-3" href="/blog">back to posts</a>
--- prakweb_c_203040171_laravel/routes/web.php
+++ prakweb_c_203040171_laravel/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\category;
 
 
 /*
@@ -50,7 +51,7 @@ Route::get('/categories', function() {
     ]);
 });
 //halaman category
-Route::get('categories/{category:slug}', function(Category $category) {
+Route::get('/categories/{category:slug}', function(Category $category) {
     return view('category', [
         'title' =>$category->name,
         'posts' =>$category->posts,
@@ -58,5 +59,16 @@ Route::get('categories/{category:slug}', function(Category $category) {
 
     ]);
 });
+
+
+//halaman User
+Route::get('/authors/{author:username}', function(User $author) {
+    return view('posts', [
+        'title' =>'User Post',
+        'posts' => $author-> posts
+
+    ]);
+});
+
 
 
