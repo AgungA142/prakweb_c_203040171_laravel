5b51cc14f7ae845ca0119616167e906cb3510b64
sacred142 <agung.alfatah43@gmail.com>
2022-11-02 00:45:44 +0700

    menyelesaikan video 17 laravel 8

--- prakweb_c_203040171_laravel/routes/web.php
+++ prakweb_c_203040171_laravel/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 
 
 
@@ -69,7 +69,12 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 //dashboard
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+//dashboard post
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 
 
